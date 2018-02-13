8142fd645f1a090ba1fffd1acf4df5bbaa7f10d0
Aaron J. Lang <aaronjameslang@googlemail.com>
2018-02-13 15:55:02 +1300

    Add more elements

--- src/App/Body/index.jsx
+++ src/App/Body/index.jsx
@@ -4,6 +4,7 @@ class Header extends React.Component {
   render () {
     return (
       <div id="body">
+      <div>Welcome, jane.smith</div>
         <label>
           I wish to report:
           <select>
@@ -11,6 +12,17 @@ class Header extends React.Component {
             <option>missed teaching</option>
           </select>
         </label>
+        <label>
+          Location:
+          <select>
+            <option>Current Location (12'34", 89'67")</option>
+            <option>St. Thomas' Hopsital</option>
+            <option>Waterloo Infirmary</option>
+          </select>
+        </label>
+        <textarea></textarea>
+        <button>Cancel</button>
+        <button>Report</button>
       </div>
     )
   }

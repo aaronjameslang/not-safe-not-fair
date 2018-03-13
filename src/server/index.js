84cb9f3e80c52dacd5571ac79cd13afc5a737fca
Aaron J. Lang <aaronjameslang@googlemail.com>
2018-03-13 17:33:00 +1300

    Add dummy get report

--- src/server/index.js
+++ src/server/index.js
@@ -2,4 +2,18 @@ const express = require('express')
 const app = express()
 
 app.use(express.static('static'))
+
+app.get('/report', (req, res) => {
+  const reports = [{
+    location: 'Sheffield',
+    type: 'understaffed',
+    details: 'blah blah blah'
+  }, {
+    location: 'St Thomas\'',
+    type: 'missed teaching',
+    details: 'blah blah blah bleeeh'
+  }]
+  res.send(reports)
+})
+
 app.listen(8080, () => console.log('Listening on 8080'))

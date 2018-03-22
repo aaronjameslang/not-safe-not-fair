92d36256d94bca91d2b88e3a594d4832d7432149
Aaron J. Lang <aaronjameslang@googlemail.com>
2018-03-22 17:53:40 +1300

    Add values to report_reason

--- migrations/sqls/20180321095909-create-table-report-reason-up.sql
+++ migrations/sqls/20180321095909-create-table-report-reason-up.sql
@@ -1,15 +1,42 @@
 CREATE TABLE report_reason (
-  id uuid NOT NULL,
+  name  varchar(15)  NOT NULL,
   label varchar(255) NOT NULL,
-  PRIMARY KEY (id)
+  PRIMARY KEY (name)
 );
 
 INSERT INTO report_reason VALUES (
-  'CAB0A6EC-B7C9-459F-ACB6-DA08190FC5D0',
-  'Blah blah blah'
+  'OVER_TIME',
+  'I worked beyond my rostered hours'
 );
 INSERT INTO report_reason VALUES (
-  '044947D0-146A-44A1-B826-A1BF273C3E9C',
-  'Blah blah blah bleh'
+  'SHORT_STAFFED',
+  'Short staffing, despite planned rota'
 );
-
+INSERT INTO report_reason VALUES (
+  'POOR_ROTA',
+  'Short staffing, poorly planned rota'
+);
+INSERT INTO report_reason VALUES (
+  'PATIENT_LOAD',
+  'Patient load exceeded capacity'
+);
+INSERT INTO report_reason VALUES (
+  'NO_SUPPORT',
+  'My colleagues were unable to support me appropriately due to their own workload'
+);
+INSERT INTO report_reason VALUES (
+  'TECH_FAILURE',
+  'There was a technical failure (IT, radiology etc)'
+);
+INSERT INTO report_reason VALUES (
+  'ILL_HEALTH',
+  'I have a health condition (mental/physical) and am not adequately supported'
+);
+INSERT INTO report_reason VALUES (
+  'MISSED_TRAINING',
+  'I missed a teaching session important to my training'
+);
+INSERT INTO report_reason VALUES (
+  'UNDER_QUALIFIED',
+  'I had to work above my level of competency because no other support was available to me'
+);

c44247ec52c21d39914d610c56e44c57b381e925
Aaron J. Lang <aaronjameslang@googlemail.com>
2018-04-10 21:59:38 +1200

    Remove scss

--- src/client/App/index.jsx
+++ src/client/App/index.jsx
@@ -1,19 +1,17 @@
 import React from 'react'
-
-import './style'
+import styled from 'styled-components'
 
 import Header from './Header'
 import Page from './Page'
+import { FONT_FAMILY } from '../theme'
 
-class App extends React.Component {
-  render () {
-    return (
-      <React.Fragment>
-        <Header />
-        <Page />
-      </React.Fragment>
-    )
-  }
-}
+export default () => (
+  <$Div className='app'>
+    <Header />
+    <Page />
+  </$Div>
+)
 
-module.exports = App
+const $Div = styled.div`
+  font-family: ${FONT_FAMILY};
+`
--- src/client/theme.js
+++ src/client/theme.js
@@ -1,3 +1,4 @@
 export const COLOUR_PRIMARY = '#005EB8'
 export const COLOUR_PRIMARY_PALE = COLOUR_PRIMARY + '20'
 export const COLOUR_WHITE = 'white'
+export const FONT_FAMILY = '"Frutiger LT W01_65 Bold1475746",Arial,Helvetica,sans-serif'

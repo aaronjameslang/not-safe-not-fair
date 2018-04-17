bc65477c3dfa944e43b305977484fc9e67621691
Aaron J. Lang <aaronjameslang@googlemail.com>
2018-04-17 12:31:40 +1200

    Typo

--- migrations/sqls/20180412044756-create-table-location-up.sql
+++ migrations/sqls/20180412044756-create-table-location-up.sql
@@ -1,7 +1,7 @@
 CREATE SCHEMA configuration;
 CREATE TABLE configuration.location (
   code char(5) NOT NULL PRIMARY KEY,
-  name varchar(35) NOT NULL,
-  addres varchar(35) NOT NULL,
+  name varchar(100) NOT NULL,
+  address varchar(35) NOT NULL,
   postcode varchar(8) NOT NULL
 );

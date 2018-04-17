78b91764f97565c0b5c626ad19c4199c452690f4
Aaron J. Lang <aaronjameslang@googlemail.com>
2018-04-17 22:16:58 +1200

    Add outcode to location

--- configuration/etrust.js
+++ configuration/etrust.js
@@ -2,47 +2,57 @@
 
 const csv = require('csv')
 const fs = require('fs')
+const { pipe } = require('ramda')
 
 const csvFile = fs.createReadStream('etrust.csv')
 const sqlFile = fs.createWriteStream('etrust.sql')
 
 let row = 1
+const prependHeaderEtc = table => columns => recordStr => {
+  if (!recordStr) return ''
 
-const transformer = record => {
-  const header = 'INSERT INTO configuration.location (code, name, address, postcode) VALUES\n'
-
-  let str = ''
+  const header = `INSERT INTO ${table} (${columns.join(', ')}) VALUES\n`
 
   if (row === 1) {
-    str += header
     row += 1
+    return header + recordStr
   } else if (row % 1000 === 0) {
-    str += ';\n'
-    str += header
     row += 2
+    return ';\n' + header + recordStr
   } else {
-    str += ',\n'
     row += 1
+    return ',\n' + recordStr
   }
+}
 
+const recordToStr = record => {
   const escape = x => x.split("'").join("''")
   const quote = x => "'" + x + "'"
   const fields = [
-    record[0], // id
+    record[0], // code
     record[1], // name
     record[4], // address
     record[9], // postcode
+    record[9].split(' ')[0], // outcode
   ].map(escape).map(quote).join(', ')
 
-  str += `(${fields})`
-
-  return str
+  return `(${fields})`
 }
+
+const transformer = pipe(
+  recordToStr,
+  prependHeaderEtc('configuration.location')([
+    'code',
+    'name',
+    'address',
+    'postcode',
+    'outcode',
+  ])
+)
 
 csvFile
   .pipe(csv.parse())
   .pipe(csv.transform(transformer))
-  .pipe(sqlFile, {end:false})
-
-csvFile.on('end', () => { sqlFile.end(';\n') })
+  .on('end', () => { sqlFile.write(';\n') })
+  .pipe(sqlFile)
 

0f8e5c3201cffc689a7dc54e51adf90225fce2d5
Aaron J. Lang <aaronjameslang@googlemail.com>
2018-04-19 16:53:04 +1200

    Fix reason select

--- src/client/App/Page/ReportNewPage.jsx
+++ src/client/App/Page/ReportNewPage.jsx
@@ -4,23 +4,28 @@ import Button from 'material-ui/Button'
 import styled from 'styled-components'
 import Select from 'material-ui/Select'
 import { MenuItem } from 'material-ui/Menu'
+import { FormControl } from 'material-ui/Form';
+import Input, { InputLabel } from 'material-ui/Input';
 
 import { getReportReasons } from '../../api'
 
-export default () => (
-  <$form>
-    <ReasonSelect label='I wish to report:' />
-    <Select value={0} floatingLabelText='Location:'>
-      <MenuItem value='1,1' >Current Location (12'34'' 89'67'')</MenuItem>
-      <MenuItem value='2,2' >Tommies</MenuItem>
-      <MenuItem value='3,3' >BRI</MenuItem>
-    </Select>
-    <TextField hintText='Details' />
-    <Button variant='raised' color='primary'>Cancel</Button>
-    <Button variant='raised' color='primary'>Report</Button>
-  </$form>
-)
-
+export default class extends React.Component {
+  render () {
+    return (
+      <$form>
+        <ReasonSelect onChange={reason => this.setState({reason})} />
+        <Select value={0} floatingLabelText='Location:'>
+          <MenuItem value='1,1' >Current Location (12'34'' 89'67'')</MenuItem>
+          <MenuItem value='2,2' >Tommies</MenuItem>
+          <MenuItem value='3,3' >BRI</MenuItem>
+        </Select>
+        <TextField hintText='Details' />
+        <Button variant='raised' color='primary'>Cancel</Button>
+        <Button variant='raised' color='primary'>Report</Button>
+      </$form>
+    )
+  }
+}
 const $form = styled.form`
   padding: 1em;
   display: flex;
@@ -30,8 +35,10 @@ const $form = styled.form`
 class ReasonSelect extends React.Component {
   constructor () {
     super()
+    this.id = Math.random()
     this.state = {
-      reasons: []
+      reasons: [],
+      value: ''
     }
     getReportReasons().then(
       reasons => {
@@ -39,11 +46,23 @@ class ReasonSelect extends React.Component {
       }
     )
   }
+  onChange (event) {
+      const { value } = event.target
+    this.setState({value})
+    this.props.onChange(value)
+  }
   render () {
     return (
-      <Select value={0} {...this.props}>
-        { this.state.reasons.map(reason => <MenuItem value={reason.name} >{reason.label}</MenuItem>)}
-      </Select>
+      <FormControl>
+        <InputLabel htmlFor={this.id}>I wish to report:</InputLabel>
+        <Select
+          inputProps={{id: this.id}}
+          onChange={::this.onChange}
+          value={this.state.value}
+        >
+          { this.state.reasons.map(reason => <MenuItem value={reason.name} >{reason.label}</MenuItem>)}
+        </Select>
+      </FormControl>
     )
   }
 }

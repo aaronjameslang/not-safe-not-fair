ad683b9022de1da2a8471033f2f3c57f25abb018
Aaron J. Lang <aaronjameslang@googlemail.com>
2018-05-09 19:05:46 +1200

    Defer auth init

--- src/client/services/auth.js
+++ src/client/services/auth.js
@@ -3,7 +3,9 @@
 import { Auth0LockPasswordless } from 'auth0-lock'
 import theme from './theme'
 
-const lock = new Auth0LockPasswordless('P4VNlmrVpFkOTfi2xM7tNoED9V9o2CAv', 'notsafenotfair.eu.auth0.com', {
+let lock
+
+const newLock = () => new Auth0LockPasswordless('P4VNlmrVpFkOTfi2xM7tNoED9V9o2CAv', 'notsafenotfair.eu.auth0.com', {
   auth: {
     redirectUrl: window.location.origin,
     responseType: 'token id_token'
@@ -14,10 +16,16 @@ const lock = new Auth0LockPasswordless('P4VNlmrVpFkOTfi2xM7tNoED9V9o2CAv', 'nots
   }
 })
 
-lock.on('authenticated', function ({idToken}) {
-  window.localStorage.setItem('token', idToken)
-})
+const getLock = () => {
+  if (!lock) {
+    lock = newLock()
+    lock.on('authenticated', function ({idToken}) {
+      window.localStorage.setItem('token', idToken)
+    })
+  }
+  return lock
+}
 
-export default ::lock.show
+export default () => getLock().show()
 
 export const getToken = () => window.localStorage.getItem('token')

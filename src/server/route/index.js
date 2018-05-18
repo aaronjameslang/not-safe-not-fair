a7c276fb227ebfab93bbbc73eea3c37074dd383a
Aaron J. Lang <aaronjameslang@googlemail.com>
2018-05-18 18:56:58 +1200

    Add first API tests

--- src/server/route/index.js
+++ src/server/route/index.js
@@ -9,7 +9,7 @@ export default api => {
   user(api)
 
   api.get('/',
-    () => fs.readFileSync('./static/index.html', 'utf8'), // TODO sync?
+    () => fs.readFileSync(process.env.HTML_URL, 'utf8'), // TODO sync?
     { success: { contentType: 'text/html' } }
   )
 }

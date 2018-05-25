ad82806b4862564ff5eac9f3cd243ce95f6b181e
Aaron J. Lang <aaronjameslang@googlemail.com>
2018-05-25 22:41:30 +1200

    Test /user

---
+++ __snapshots__/user.spec.js
@@ -0,0 +1,41 @@
+exports['user.spec.js GET /user 200 1'] = {
+  "statusCode": 200,
+  "headers": {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "Content-Type,Authorization,X-Amz-Date,X-Api-Key,X-Amz-Security-Token",
+    "Access-Control-Allow-Methods": "GET,OPTIONS",
+    "Access-Control-Allow-Credentials": "true",
+    "Access-Control-Max-Age": "0"
+  },
+  "body": {
+    "emailAddress": "legit@nhs.net",
+    "id": "[REDACTED:string:36]"
+  }
+}
+
+exports['user.spec.js GET /user 401 1'] = {
+  "statusCode": 401,
+  "headers": {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "Content-Type,Authorization,X-Amz-Date,X-Api-Key,X-Amz-Security-Token",
+    "Access-Control-Allow-Methods": "GET,OPTIONS",
+    "Access-Control-Allow-Credentials": "true",
+    "Access-Control-Max-Age": "0"
+  },
+  "body": "jwt expired"
+}
+
+exports['user.spec.js GET /user 403 1'] = {
+  "statusCode": 403,
+  "headers": {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "Content-Type,Authorization,X-Amz-Date,X-Api-Key,X-Amz-Security-Token",
+    "Access-Control-Allow-Methods": "GET,OPTIONS",
+    "Access-Control-Allow-Credentials": "true",
+    "Access-Control-Max-Age": "0"
+  },
+  "body": {}
+}
--- src/server/route/user.js
+++ src/server/route/user.js
@@ -1,4 +1,9 @@
 import auth from '../services/auth'
 export default api => {
-  api.get('/user', ({ headers }) => auth(headers))
+  api.get('/user', ({ headers }) => auth(headers).catch(errorHandler(api)))
+}
+
+const errorHandler = api => error => {
+  console.log(error)
+  return new api.ApiResponse(error.message, {}, error.httpStatusCode || 500)
 }
